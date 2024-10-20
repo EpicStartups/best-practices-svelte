ea7d9e6b6abe43c833de77eb9af9c987f9152379
IcelandicIcecream <benjaminsinidol@gmail.com>
2024-10-20 17:45:09 +0800

    testing svelte best practices

--- src/routes/friends/+page.server.ts
+++ src/routes/friends/+page.server.ts
@@ -1,10 +1,13 @@
-export function load() {
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
 	return {
 		hobbies: [
 			{
 				name: 'swimming',
 				skillLevel: 10
 			}
-		]
+		],
+		name: locals.name
 	};
-}
+};
